e354d55c587ce44d64c6f3137dd1e4dc389292a0
prerna-khanna <prerna.khanna97@gmail.com>
2025-01-28 20:41:56 +0000

    categories of gesture added

--- utils.py
+++ utils.py
@@ -402,7 +402,10 @@ class LIBERTDataset4Pretrain(Dataset):
 def handle_argv(target, config_train, prefix):
     parser = argparse.ArgumentParser(description='PyTorch LIMU-BERT Model')
     parser.add_argument('model_version', type=str, help='Model config')
-    parser.add_argument('dataset', type=str, help='Dataset name', choices=['hhar', 'motion', 'uci', 'shoaib', 'blind_user', 'sighted_user', 'blind_fine_tune', 'augmented_data', 'test_data'])
+    parser.add_argument('dataset', type=str, help='Dataset name', choices=['hhar', 'motion', 'uci', 'shoaib', 'blind_user', 'sighted_user', 
+                                                                           'blind_fine_tune', 'augmented_data', 'test_data', 'blind_access_wear', 
+                                                                           'blind_shape', 'blind_compound'])
+                                                                           
     parser.add_argument('dataset_version',  type=str, help='Dataset version', choices=['10_100', '20_120', '20_40'])
     parser.add_argument('-g', '--gpu', type=str, default=None, help='Set specific GPU')
     parser.add_argument('-f', '--model_file', type=str, default=None, help='Pretrain model file')
